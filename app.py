aa7f169435964ae2969bc71a72b499229c197d4c
SeifAlsayed02 <seifalsayed0202@gmail.com>
2024-05-22 23:58:40 +0300

    adujtments after deployment

--- app.py
+++ app.py
@@ -13,16 +13,10 @@ def index():
 @app.route('/heart_rate', methods=['POST'])
 def receive_heart_rate():
     data = request.get_json()
-    if all(key in data for key in ("patient_id", "heart_rate", "timestamp")):
-        try:
-            data["timestamp"] = datetime.strptime(data["timestamp"], "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
-            heart_rate_data.append(data)
-            print(f"Received data: {data}\n")
-            return jsonify({"status": "success"}), 200
-        except ValueError:
-            return jsonify({"status": "error", "message": "Invalid timestamp format"}), 400
-    else:
-        return jsonify({"status": "error", "message": "Invalid data format"}), 400
+    data['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    heart_rate_data.append(data)
+    print(f"Received data: {data}\n")
+    return jsonify({"status": "success"}), 200
 
 @app.route('/heart_rate', methods=['GET'])
 def get_heart_rate():
